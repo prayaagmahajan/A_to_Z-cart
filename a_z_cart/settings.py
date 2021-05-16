6233e5d6680f13ac530ad995047881a90af0484a
prayaagmahajan <64199797+prayaagmahajan@users.noreply.github.com>
2021-05-16 23:43:45 +0530

    Update settings.py

--- a_z_cart/settings.py
+++ a_z_cart/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5@oicry!5lty@dw@&9prrmqd62t&9j3rorppbxh!50chmky@6-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
